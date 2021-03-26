eb5bf43b9a26da38cd549e51b1691fc88a695cba
Shravan Srinivasan <sshravan@cs.umd.edu>
2021-03-26 01:06:53 -0400

    Multi-exp numbers

--- main_bench_test.go
+++ main_bench_test.go
@@ -19,12 +19,78 @@ func inputs() []Input {
 		{100_000, 100_000, 2},
 		{500_000, 500_000, 2},
 		{1_000_000, 1_000_000, 2},
-		{7_648_002, 7_648_002, 2},
+		// {7_648_002, 7_648_002, 2},
 		// {14_018_002, 14_018_002, 2},
 		// {20_388_002, 20_388_002, 2},
 		// {21_964_002, 21_964_002, 2},
 	}
 	return data
+}
+
+func BenchmarkExponentiation(b *testing.B) {
+
+	var size []uint64
+	size = []uint64{100_000}
+	for i := 0; i < len(size); i++ {
+		baseG1 := generateG1(size[i])
+		baseG2 := generateG2(size[i])
+		expoFr := generateFr(size[i])
+		fmt.Println("Done generating the data")
+
+		b.Run(fmt.Sprintf("%d/G1Add;", size[i]), func(t *testing.B) {
+			var result mcl.G1
+			result.SetString("1", 10)
+			t.ResetTimer()
+			for i := 0; i < t.N; i++ {
+				for j := 0; j < len(baseG1); j++ {
+					mcl.G1Add(&result, &result, &baseG1[j])
+				}
+			}
+		})
+		b.Run(fmt.Sprintf("%d/G1Mul;", size[i]), func(t *testing.B) {
+			var result mcl.G1
+			t.ResetTimer()
+			for i := 0; i < t.N; i++ {
+				for j := 0; j < len(expoFr); j++ {
+					mcl.G1Mul(&result, &baseG1[j], &expoFr[j])
+				}
+			}
+		})
+		b.Run(fmt.Sprintf("%d/G1MulVec;", size[i]), func(t *testing.B) {
+			var result mcl.G1
+			t.ResetTimer()
+			for i := 0; i < t.N; i++ {
+				mcl.G1MulVec(&result, baseG1, expoFr)
+			}
+		})
+
+		b.Run(fmt.Sprintf("%d/G2Add;", size[i]), func(t *testing.B) {
+			var result mcl.G2
+			result.SetString("1", 10)
+			t.ResetTimer()
+			for i := 0; i < t.N; i++ {
+				for j := 0; j < len(baseG1); j++ {
+					mcl.G2Add(&result, &result, &baseG2[j])
+				}
+			}
+		})
+		b.Run(fmt.Sprintf("%d/G2Mul;", size[i]), func(t *testing.B) {
+			var result mcl.G2
+			t.ResetTimer()
+			for i := 0; i < t.N; i++ {
+				for j := 0; j < len(expoFr); j++ {
+					mcl.G2Mul(&result, &baseG2[j], &expoFr[j])
+				}
+			}
+		})
+		b.Run(fmt.Sprintf("%d/G2MulVec;", size[i]), func(t *testing.B) {
+			var result mcl.G2
+			t.ResetTimer()
+			for i := 0; i < t.N; i++ {
+				mcl.G2MulVec(&result, baseG2, expoFr)
+			}
+		})
+	}
 }
 
 func BenchmarkGroth16(b *testing.B) {
